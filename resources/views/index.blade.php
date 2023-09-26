7366deb599903caf04b80b91e9ae9244bb118cd8
SalvatoreBono <salvatore.bono2001@libero.it>
2023-09-26 16:16:40 +0200

    finish ex

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.public')
 @section('content')
-    @dump($Movies)
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4"">
+            @foreach ($Movies as $item)
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{ $item['title'] }}</li>
+                            <li class="list-group-item">{{ $item['original_title'] }}</li>
+                            <li class="list-group-item">{{ $item['nationality'] }}</li>
+                            <li class="list-group-item">{{ $item['date'] }}</li>
+                            <li class="list-group-item">{{ $item['vote'] }}</li>
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
